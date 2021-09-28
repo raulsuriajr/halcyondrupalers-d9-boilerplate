74ff350daa7f11c5a5de06f7b9d7a4b83469f31c
raulsuriajr <raul.halcyondigital@gmail.com>
2021-09-28 10:52:14 +0800

    added webform_bootstrap in enmod of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 case $1 in
 "enmod")
-  php drush/drush en module_filter backup_migrate admin_toolbar adminimal_admin_toolbar admin_toolbar_tools admin_toolbar_search pathauto metatag twig_tweak twig_extensions twig_field_value coffee bamboo_twig bamboo_twig_config bamboo_twig_extensions bamboo_twig_file bamboo_twig_i18n bamboo_twig_loader bamboo_twig_security bamboo_twig_path bamboo_twig_token editor_advanced_link editor_advanced_image back_to_top image_effects imageapi_optimize_resmushit advagg advagg_css_minify advagg_js_minify quicklink webform webform_node webform_submission_log webform_ui
+  php drush/drush en module_filter backup_migrate admin_toolbar adminimal_admin_toolbar admin_toolbar_tools admin_toolbar_search pathauto metatag twig_tweak twig_extensions twig_field_value coffee bamboo_twig bamboo_twig_config bamboo_twig_extensions bamboo_twig_file bamboo_twig_i18n bamboo_twig_loader bamboo_twig_security bamboo_twig_path bamboo_twig_token editor_advanced_link editor_advanced_image back_to_top image_effects imageapi_optimize_resmushit advagg advagg_css_minify advagg_js_minify quicklink webform webform_node webform_submission_log webform_ui webform_bootstrap
   ;;
 "ensub")
   php drush/drush then bootstrap_sub
